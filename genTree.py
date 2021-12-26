641df05d85ec5f94c06121a6a18eef222ef185d8
dudupo <dudupo51@gmail.com>
2021-12-26 14:01:58 +0200

    set up NJ

--- genTree.py
+++ genTree.py
@@ -22,10 +22,11 @@ def generateTree(dist):
 	
 	def buildQ_matrixR_matrix(dim):
 		r_matrix = np.sum(D, axis=0) / (n-2) 
-		Q_matrix = np.zeros((dim,dim))
+		Q_matrix = np.ones((dim,dim)) * - np.inf
 		for i in range(dim):
-			for j in range(dim):
+			for j in range(i+1, dim):
 				Q_matrix[i][j] = r_matrix[i] + r_matrix[j] - D[i][j] 
+		# print(Q_matrix)
 		return Q_matrix, r_matrix
 	
 	newnode = None
@@ -33,24 +34,47 @@ def generateTree(dist):
 		Q_matrix, r_matrix = buildQ_matrixR_matrix(k)
 		i,j = np.unravel_index(
 			np.argmax(Q_matrix), Q_matrix.shape)
+		# print(i,j)
 		newnode = connect(leafs[i], leafs[j])
+		
+		
+		# leafs.pop(i)
+		# leafs.pop(j)
 		leafs.append(newnode)
 		
 		for m in range(k):
-			D[k][m] = 0.5*(D[i][m] + D[j][m] - D[i][j])  
-		
+			D[m][k] = 0.5*(
+				 max(D[i][m], D[m][i]) +\
+				 max(D[j][m], D[m][j])  -\
+				 max(D[i][j], D[j][i]))
+			D[k][m] = 0
 		k += 1
 
 	print(newnode)
-	# should be the root 
+	
+	def dfs(_newnode, _str):
+		if _newnode is None:
+			return
+		dfs(_newnode.left, _str + " ")
+		print(_str + "hi")
+		dfs(_newnode.right, _str + " ")
+
+	dfs(newnode, "")
 	return newnode
-	print(i,j)
+	
 
 def test():
 	dist = np.array([
-		[1 , 2 ,3],
-		[4 , 6 ,3],
-		[1 , 2 ,7]])
+		[1 , 2 ,3, 2, 2, 9],
+		[4 , 6 ,3, 4, 2, 9],
+		[1 , 2 ,7, 1, 2, 9],
+		[1 , 2 ,7, 1, 2, 9],
+		[1 , 2 ,7, 1, 2, 9],
+		[1 , 2 ,7, 1, 2, 9]], dtype=np.float32)
+	n = dist.shape[0]
+	for i in range(n):
+		for j in range(i,n):
+			dist[j][i] = 0
 	generateTree(dist)
 if __name__ == "__main__":
 	test()
